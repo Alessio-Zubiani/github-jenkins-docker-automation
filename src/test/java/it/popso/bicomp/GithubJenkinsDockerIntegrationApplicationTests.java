237bfac2eb52779bf1fd05acd5cdb81671a6e573
alessiozubiani <alessio.zubiani1996@gmail.com>
2023-05-26 17:06:09 +0200

    Repository initialization

--- src/test/java/it/popso/bicomp/GithubJenkinsDockerIntegrationApplicationTests.java
+++ src/test/java/it/popso/bicomp/GithubJenkinsDockerIntegrationApplicationTests.java
@@ -1,13 +1,17 @@
 package it.popso.bicomp;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class GithubJenkinsDockerIntegrationApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
